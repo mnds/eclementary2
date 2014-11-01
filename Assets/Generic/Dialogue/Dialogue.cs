f5b1a3cbc3212a92e55c26276aad6029782ea85b
baptiste.collette <baptiste.collette@ecl13.ec-lyon.fr>
2014-11-01 02:19:00 +0100

    test enchainement repliques

--- Assets/Generic/Dialogue/Dialogue.cs
+++ Assets/Generic/Dialogue/Dialogue.cs
@@ -13,17 +13,60 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Dialogue {
+public class Dialogue : MonoBehaviour {
 	public List<Replique> repliquesObjet; //Liste de toutes les repliques de l'objet
 	List<Replique> repliquesAccessibles;
 	Replique repliqueActuelle; //Replique en train d'etre lue par l'objet
 
-	void AddReplique (Replique replique) {
+	void Start () {
+		repliquesAccessibles = new List<Replique> ();
+		foreach (Replique replique in repliquesObjet) {
+			replique.SetDialogue (this);
+			if(replique.GetAccessible()) //Si la replique est accessible
+				AddRepliqueAccessible(replique);
+		}
+	}
+
+	//Ajout de repliques à l'objet par code
+	public void AddReplique (Replique replique) {
 		repliquesObjet.Add (replique);
+		if (replique.GetDialogue () != null) //Si la réplique avait un dialogue avant
+						replique.GetDialogue ().RemoveReplique (replique); //On lui enlève
+		replique.SetDialogue (this); //Et on change le dialogue
+	}
+
+	//Appelé par AddReplique. Donc rien d'autre à faire que d'enlever la réplique.
+	public void RemoveReplique (Replique replique) {
+		repliquesObjet.Remove (replique);
+	}
+
+	//Ajout de repliques accessibles
+	public void AddRepliqueAccessible (Replique replique) {
+		replique.SetAccessible (true);
+		repliquesAccessibles.Add (replique);
+	}
+	
+	public void RemoveRepliqueAccessible (Replique replique) {
+		replique.SetAccessible (false);
+		repliquesAccessibles.Remove (replique);
 	}
 
 
+	public void ArreterRepliqueActuelle () {
+		if(!repliqueActuelle) return; //S'il n'y a pas de replique actuelle, on n'a rien à faire
+		repliqueActuelle.ArreterLecture (); //On arrete la lecture
+		repliqueActuelle = null; //Plus de réplique actuelle
+	}
 
+	public void LancerReplique(Replique replique) {
+		if(repliqueActuelle) return; //Si une replique est déjà en cours pour cet objet, on abandonne.
+		if(!replique.GetAccessible()) return; //Si la replique n'est pas accessible, on ne peut pas la lancer
+
+		repliqueActuelle = replique;
+		replique.Lire ();
+	}
+
+	//Setters/Getters
 	public void SetRepliquesObjet (List<Replique> repliquesObjet_) {
 		repliquesObjet = repliquesObjet_;
 	}
@@ -47,5 +90,5 @@ public class Dialogue {
 	public Replique GetRepliqueActuelle () {
 		return repliqueActuelle;
 	}
-
 }
+ 
--- Assets/Generic/Dialogue/Replique.cs
+++ Assets/Generic/Dialogue/Replique.cs
@@ -7,6 +7,8 @@
  *
  * \details   Contient le texte d'une réplique, un son qui va avec, quelles autres répliques sont enclenchées par l'activation de cette réplique,
  * 		      et quels éléments sont affectés par cette réplique.
+ * 			  La réplique ne tient pas en compte de quelle réplique l'a déclenchée. La réplique du personnage joué et la réponse d'un objet sont deux répliques différentes.
+ * 			  Les répliques disponibles pour le joueur sont intégralement décidées dans DialogueManager.cs.
  */
 
 using UnityEngine;
@@ -14,46 +16,108 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Replique : MonoBehaviour {
-	bool accessible; //Si la replique a le droit d'etre utilisée
 	bool enCours; //Si la replique est en train d'etre utilisee
 	public string texteReplique; //Le texte associé à la réplique
 	public AudioSource sourceDeLaReplique; //D'où le son produit par la réplique va venir
 	public List<AudioClip> sonReplique; //Le son associé à la réplique. C'est une liste au cas où on voudrait mettre par exemple plusieurs intonations.
 	AudioClip sonRepliqueActuel;
 
-	bool desactivationAutomatique = false; //true si la réplique est désactivée dès que le son est arrete. Attention, il faut un clip sonore associé !
+	public bool desactivationAutomatique = false; //true si la réplique est désactivée dès que le son est arrete. Attention, il faut un clip sonore associé !
 	float tempsDepuisLecture = 0;
 
+	public bool accessibleDebut; //Au début, est-ce que la réplique est accessible
+	bool accessible; //Si la replique a le droit d'etre utilisée
+	public List<Replique> repliquesRenduesAccessibles; //Après activation de la replique, des repliques sont rendues accessibles
+	public List<Replique> repliquesRenduesInaccessibles; //Après activation de la replique, des repliques sont rendues inaccessibles
+	Dialogue dialogue; //Le dialogue auquel est associé la réplique. Est obligatoire.
 
+	public Replique repliqueSuivante; //Si une réplique doit etre declenchee juste apres, c'est ici qu'elle est stockée
+
+	void Start () {
+		/*	Test pour vérifier que les repliques s'enchainent. Prendre une replique activable au début, desactivation auto, qui rend accessible une nouvelle replique, qui est aussi la replique suivante.
+		 *  Mettre ces deux répliques dans un dialogue. Tester avec les deux lignes qui suivent. */
+		if (accessibleDebut)
+			Lire ();
+		/* fin test */
+	}
+
+	void Awake () {
+		accessible = accessibleDebut;
+	}
 
 	void Update () {
 		if (!accessible) return; //Si ce n'est pas accessible, on laisse tomber
 		//Si le son est en cours, on désactive le tout une fois que le son est terminé
-		if(desactivationAutomatique && sonRepliqueActuel)
+		if(desactivationAutomatique && sonRepliqueActuel && enCours)
 		{
 			tempsDepuisLecture+=Time.deltaTime;
 			if(tempsDepuisLecture>sonRepliqueActuel.length)
-				ArreterLecture();
+				dialogue.ArreterRepliqueActuelle();
 		}
 	}
 
 	public void Lire () {
 		if (!accessible) return; //Si ce n'est pas accessible, on laisse tomber
 
-		enCours = true;
-		tempsDepuisLecture = 0;
-		int sonLu = Random.Range (0, sonReplique.Count);
-		sonRepliqueActuel = sonReplique [sonLu];
+		enCours = true; //Son en cours
+		dialogue.SetRepliqueActuelle (this); //On dit au dialogue qu'on est la réplique actuelle
+		tempsDepuisLecture = 0; //Remise à 0
+		int sonLu = Random.Range (0, sonReplique.Count); //On choisir un son associé à la réplique
+		sonRepliqueActuel = sonReplique [sonLu]; //On le stocke
 		if(sonRepliqueActuel && sourceDeLaReplique) //Si le son et la source existent, on le joue
 			sourceDeLaReplique.PlayOneShot (sonRepliqueActuel);
 	}
 
+	//Arrete la lecture de l'objet ; le son associé est coupé, et on effectue les actions nécessaires à continuer le dialogue.
 	public void ArreterLecture () {
 		if (!accessible) return; //Si ce n'est pas accessible, on laisse tomber
 
-		if(sourceDeLaReplique)
+		if(sourceDeLaReplique) //Le son est arreté
 			sourceDeLaReplique.Stop ();
-		enCours = false;
+		enCours = false; //La réplique n'est plus en cours
+		dialogue.SetRepliqueActuelle (null); //Plus de réplique actuelle
+
+		FinirReplique (); //On fait tout ce qui est associé à la réplique
+	}
+
+	public void FinirReplique () {
+		//Changer les flags des répliques associées
+		RendreRepliquesAccessibles ();
+		RendreRepliquesInaccessibles ();
+		if (repliqueSuivante)
+			repliqueSuivante.Lire ();
+	}
+
+	public void AddRepliqueAccessible (Replique replique) {
+		repliquesRenduesAccessibles.Add (replique);
+	}
+
+	public void RemoveRepliqueAccessible (Replique replique) {
+		repliquesRenduesAccessibles.Remove (replique);
+	}
+
+	public void AddRepliqueInaccessible (Replique replique) {
+		repliquesRenduesInaccessibles.Add (replique);
+	}
+
+	public void RemoveRepliqueInaccessible (Replique replique) {
+		repliquesRenduesInaccessibles.Remove (replique);
+	}
+
+	public void RendreRepliquesAccessibles () {
+		foreach (Replique replique in repliquesRenduesAccessibles)
+		{
+			replique.SetAccessible(true);
+			replique.GetDialogue().AddRepliqueAccessible(replique);
+		}
+	}
+
+	public void RendreRepliquesInaccessibles () {
+		foreach (Replique replique in repliquesRenduesInaccessibles)
+		{
+			replique.SetAccessible(false);
+			replique.GetDialogue().RemoveRepliqueAccessible(replique);
+		}
 	}
 
 	//Setters/Getters
@@ -98,4 +162,27 @@ public class Replique : MonoBehaviour {
 		return sonReplique;
 	}
 
+	public void SetDialogue (Dialogue dialogue_) {
+		dialogue = dialogue_;
+	}
+	
+	public Dialogue GetDialogue () {
+		return dialogue;
+	}
+
+	public void SetDesactivationAutomatique (bool desactivationAutomatique_) {
+		desactivationAutomatique = desactivationAutomatique_;
+	}
+	
+	public bool GetDesactivationAutomatique () {
+		return desactivationAutomatique;
+	}
+
+	public void SetRepliqueSuivante (Replique repliqueSuivante_) {
+		repliqueSuivante = repliqueSuivante_;
+	}
+	
+	public Replique GetRepliqueSuivante () {
+		return repliqueSuivante;
+	}
 }
