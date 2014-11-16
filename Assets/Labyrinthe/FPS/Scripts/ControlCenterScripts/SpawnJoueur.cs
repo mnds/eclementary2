55a821117ccdaedc62ae41d26fd73b74250f3657
Zepeng <sddzlizepeng@gmail.com>
2014-11-16 20:52:40 +0100

    Roadfinding

---
+++ Assets/Labyrinthe/FPS/Scripts/ControlCenterScripts/SpawnJoueur.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * \file      SpawnJoueur.cs
+ * \author    
+ * \version   1.0
+ * \date      9 novembre 2014
+ * \brief     Point de départ du joueur dans la scène.
+ *
+ * \details   Instantie le joueur donné en attribut au point de départ choisi.
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class SpawnJoueur : MonoBehaviour {
+	public GameObject pointDeDepartJoueur;
+	public GameObject joueur;
+	// Use this for initialization
+	void Start () {
+		Instantiate (joueur, pointDeDepartJoueur.transform.position, pointDeDepartJoueur.transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/SceneTests/FPS/Roadfinding_Enemy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Roadfinding_Enemy : MonoBehaviour {
+
+	//Component pour trouver le chemin
+	NavMeshAgent m_agent;
+	GameObject m_player;
+	Transform m_transform;
+
+	//vitesse de mouvement
+	float m_movSpeed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+		m_transform = this.transform;
+		m_agent = this.GetComponent<NavMeshAgent> ();
+
+		// localiser Player!
+		m_player = GameObject.FindGameObjectWithTag ("Player");
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		//Definir la destination de la recherche de chemin
+		m_agent.SetDestination (m_player.transform.position);
+
+	
+		float speed = m_movSpeed * Time.deltaTime;
+		m_agent.Move (m_transform.TransformDirection (new Vector3 (0, 0, speed)));
+
+	}
+
+
+}
