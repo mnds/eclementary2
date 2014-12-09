4af6b9b47d96653e614a474c238fa3b6601ae537
baptiste.collette <baptiste.collette@ecl13.ec-lyon.fr>
2014-12-09 18:45:21 +0100

    merge

---
+++ Assets/Generic/ActiverFlag.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiverFlag : MonoBehaviour, Interactif {
+	public float distanceMinimaleInteraction = 4.0f; //La distance à laquelle on doit etre pour pouvoir interagir avec l'objet
+	public int flagActive = 0;
+
+	public void DemarrerInteraction() {
+		
+	}
+
+	public void ArreterInteraction() {
+
+	}
+
+	public void SetDistanceMinimaleInteraction (float distanceMinimaleInteraction_)
+	{
+		distanceMinimaleInteraction = distanceMinimaleInteraction_;
+	}
+
+	public float GetDistanceMinimaleInteraction ()
+	{
+		return distanceMinimaleInteraction;
+	}
+}
