82398967f1e2f14dd3fd3bef4d01da408b37fb70
Zepeng <sddzlizepeng@gmail.com>
2014-12-31 20:39:58 +0100

    ajout des zombie dans la scene 'Compus Exterieur Modifie Par Amaury'

---
+++ Assets/Generic/Joueurs/Scripts/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * \file      Scoremananger.cs
+ * \author    Zepeng LI
+ * \version   1.0
+ * \date      24 decembre 2014
+ * \brief     Contient les statistiques du zombie sur lequel ce script est attaché.
+ *
+ * \details   Herite de Caracteristiques, Contient les caractéristiques naturelles du zombie (bonus de vie, endurance, attaque, défense...)
+ * 			  ainsi que son état (empoisonné, brulé...) qui peuvent affecter l'expérience de jeu.
+ */
+
+/*
+ * Utilisé dans Attaquer , Health , ControllerJoueur
+ */
+using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+	public int score=0;
+
+	// get/set
+	public int GetScore ()
+	{
+		return score;
+	}
+	public void SetScore(int _score)
+	{
+		score=_score;
+	}
+	public void AjouteScore(int _score)
+	{
+		score += _score;
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (0, Screen.height - 50, 100, 50), "Score : " + score);
+	}
+}
---
+++ Assets/Zombie/ZombieScripts/ZombieSousAttaque.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * \file      ZombieSousAttaque.cs
+ * \author    ZepengLI
+ * \version   1.0
+ * \date      16 decembre 2014
+ * \brief     Calculer les degats subis par le zombie si il est attaque par l'arme du joueur.
+ *
+ * \details  
+ */
+
+/*
+ * 
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class ZombieSousAttaque : MonoBehaviour {
+
+void OntriggerEnter (Collider other)
+	{ 
+		Attaquer m_attaquer = other.GetComponent<Attaquer>();
+		ZombieHealth healthZombie = this.GetComponent<ZombieHealth>(); 
+		
+		if (m_attaquer) {  //Si l'object qui rentre contient le script "Attaquer"
+			healthZombie.SubirDegats(m_attaquer.degatsParCoup);	
+			}
+	}
+
+}
