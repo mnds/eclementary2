8845faf7d247c6182d16d407d33f12d54c48b984
baptiste.collette <baptiste.collette@ecl13.ec-lyon.fr>
2015-01-20 10:14:59 +0100

    ajout de la chambre et des scripts de désactivation des objets

---
+++ Assets/Generic/Scripts/Flags/ActivationSelonFlags.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * \file      ActivationSelonFlags.cs
+ * \author    
+ * \version   1.0
+ * \date      19 janvier 2015
+ * \brief     Attaché aux objets dont l'activation dépend de l'état des flags
+ */
+
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActivationSelonFlags : MonoBehaviour {
+	public List<int> flagsDevantEtreActives;
+	public List<int> flagsDevantEtreDesactives;
+
+	void Start () {
+		ControlCenter.AddObjetActivableSelonFlags(this,Application.loadedLevelName);
+		Verifier();
+	}
+
+	/**
+	 * @brief Verifie que les flags des deux listes en attribut sont bien aux bons états
+     */
+	public void Verifier () {
+		foreach(int idFlagA in flagsDevantEtreActives) {
+			if(!FlagManager.ChercherFlagParId(idFlagA).actif) { //Pas activé, problème
+				gameObject.SetActive(false); //On désactive le gO
+				return; //ca ne sert plus à rien
+			}
+		}
+		foreach(int idFlagA in flagsDevantEtreDesactives) {
+			if(FlagManager.ChercherFlagParId(idFlagA).actif) { //Pas activé, problème
+				gameObject.SetActive(false); //On désactive le gO
+				return; //ca ne sert plus à rien
+			}
+		}
+		//Tout est bon
+		gameObject.SetActive(true);
+	}
+}
---
+++ Assets/Generic/Scripts/Flags/ChangementSceneFlag.cs
@@ -0,0 +1,80 @@
+﻿/**
+ * \file      ChangementSceneFlag.cs
+ * \author    
+ * \version   1.0
+ * \date      25 décembre 2014
+ * \brief     Script permettant à un GameObject d'activer un flag à l'aide du script static FlagManager, puis
+ * 			  de changer de scène.
+ * 
+ * \details	  Similaire à ActivationFlag
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChangementSceneFlag : MonoBehaviour {
+	[System.Serializable]
+	public class FlagsRequisInterditsChangementSceneFlag { //Pour les déclarations publiques. Uniquement pour empecher l'inspecteur de Unity de faire n'importe quoi.
+		public string nomScene; //Nom de la scène à laquelle accéder
+		public List<int> flagsNecessairesPourTransition;
+		public List<int> flagsInterditsPourTransition;
+		public string nomDuSpawnPoint; //Nom du spawnPoint sur lequel on doit se téléporter
+	}
+	
+	public List<int> listeDesFlagsPouvantEtreActives; //Liste de tous les flags qu'on doit tenter d'activer avant de changer de scène
+	public List<bool> activationPossibleDesFlags; //Pour éviter d'activer tous les flags plein de fois
+	public List<FlagsRequisInterditsChangementSceneFlag> nomsDesScenesAccessibles; //Noms de toutes les scènes auxquelles on peut accéder.
+	
+	/**
+	* @brief Active les flags puis change de scène.
+	* 
+	* @details Commence par vérifier si l'activation du flag est lié à des paramètres extérieurs (inventaire...)
+    */
+	public void ActivationFlagActive () {
+		//Activation des flags
+		for(int i=0;i<listeDesFlagsPouvantEtreActives.Count;i++) {
+			int idFlag = listeDesFlagsPouvantEtreActives[i];
+			TestsActivation(idFlag); //Tests concernant l'inventaire / les caractéristiques du joueur... qui peuvent changer l'état de activationPossibleDesFlags[idFlag] et le mettre à false.
+			if(activationPossibleDesFlags[i]) {//Si on a le droit de l'activer
+				//Si le flag est activé, plus besoin de pouvoir l'activer. On désactive le script.
+				activationPossibleDesFlags[i] = !FlagManager.ActiverFlag(idFlag);
+				if(!activationPossibleDesFlags[i]) {
+					FaireActionsConnexes(idFlag); //Tout ce qui concerne uniquement cette interaction (appel à des événements...)
+				}
+			}
+		}
+		foreach(int idFlag in listeDesFlagsPouvantEtreActives) { //On fait tous les flags
+
+		}
+
+		//Changement de scène
+		foreach(FlagsRequisInterditsChangementSceneFlag fricsf in nomsDesScenesAccessibles) {
+			bool changerDeScene = true; //Peut changer pendant les tests
+			foreach (int fnpt in fricsf.flagsNecessairesPourTransition) {
+				if (!FlagManager.ChercherFlagParId (fnpt).actif) {
+					changerDeScene=false;
+				}
+			}
+			foreach (int fipt in fricsf.flagsInterditsPourTransition) {
+				if (FlagManager.ChercherFlagParId (fipt).actif) {
+					changerDeScene=false;
+				}
+			}
+			if(changerDeScene) { //Tous les flags sont bons, on change de scène.
+				ControlCenter.SetNomSpawnPointActuel(fricsf.nomDuSpawnPoint);
+				Application.LoadLevel(fricsf.nomScene);
+			}
+		}
+	}
+
+	//Destinée à etre override
+	public virtual void TestsActivation(int idFlag_) {
+		
+	}
+	
+	//Destinée à etre override, actions à faire selon l'id. On met un switch dans la méthode pour savoir qui fait quoi
+	public virtual void FaireActionsConnexes(int idFlag_) {
+		
+	}
+}
