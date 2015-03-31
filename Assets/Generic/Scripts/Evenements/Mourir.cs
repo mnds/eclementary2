4f95cb9906741292615a4f1cea923902e18911b4
mnds <mndslin@gmail.com>
2015-03-31 14:24:33 +0200

    [Corrections] 1) Résurection après avoir tué un seul des 2 boss finaux (mnds) 2) Affichage de la barre des caractéristiques sur tous les écrans (Amaury)

--- Assets/Generic/Scripts/Evenements/Mourir.cs
+++ Assets/Generic/Scripts/Evenements/Mourir.cs
@@ -8,6 +8,16 @@ public class Mourir : Evenement {
 		ControlCenter.GetJoueurPrincipal ().GetComponent<ControllerJoueur>().setEnabled (false);
 		StateManager.getInstance().etatActif.DesactiverEtat(); // Désactivation de l'état précédent
 		StateManager.getInstance().BasculerEtat( new EtatMort( StateManager.getInstance() ) );
+
+		// Cas où le joueur est mort est lors d'un combat avec le boss final
+		FlagManager.Flag flag431 = FlagManager.ChercherFlagParId (431);
+		FlagManager.Flag flag435 = FlagManager.ChercherFlagParId (435);
+		FlagManager.Flag flag436 = FlagManager.ChercherFlagParId (436);
+		if (flag431.actif && (flag435.actif ^ flag436.actif)) {
+			flag431.actif = false;
+			flag435.actif = false;
+			flag436.actif = false;
+		}
 		//StateManager stateManager = StateManager.getInstance ();
 		//Debug.Log ( "Mort: Scène de l'ancien état " + stateManager.ancienEtat.getSceneCorrespondante());
 	}
